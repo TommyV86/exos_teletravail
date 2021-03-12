67d6a85327c85e817440633362db5a9f2d93d883
Tommy Vichidvongsa <tommychel@hotmail.fr>
2021-03-12 17:57:35 +0100

    maj exo 6-13 php

--- PHP/6_13.php
+++ PHP/6_13.php
@@ -3,21 +3,26 @@
 
 function tableau()
 {
+    $tab = [];
+    
+    $inputTabBoxes = readline("enter boxes number : ");
+    $tab[] = $inputTabBoxes;
+    for ($i = 0; $i < $inputTabBoxes; $i++) {
+        $inputNumbInBoxes = readline("enter a number : ");
+        $tab[$i] = $inputNumbInBoxes;
+    }
+    print_r($tab);
+    echo tabCompare($tab);
+}
 
-    $tab = array();
-    for ($i = 0; $i < 5; $i++) {
-        $greaterNumb = 0;
-        $input = readline("enter a number : ");
-        $tab[] = $input;
-        for ($j = 1; $j < 5; $j++) {
-            if ($tab[$i] > $tab[$j] - 1) {
-                $greaterNumb += $tab[$i];
-            }
+function tabCompare($arr){
+    $greaterNumb = 0;
+    for ($j = 0; $j < count($arr); $j++) {
+        if ($arr[$j] > $arr[0]){
+            $arr[0] = $arr[$j];
+            $greaterNumb = $arr[$j];
         }
     }
-
-    print_r($tab);
-    echo "\n";
     echo $greaterNumb;
 }
 
